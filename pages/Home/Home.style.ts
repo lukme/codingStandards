2e054e4ac370fc588717c4df01575a81051e6c0c
lukasz.mecfal <lukasz.mecfal@hycom.pl>
2020-08-10 17:10:55 +0200

    cleared code, created basic structure

---
+++ pages/Home/Home.style.ts
@@ -0,0 +1,14 @@
+import * as RN from 'react-native';
+
+export default RN.StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  } as RN.ViewStyle,
+
+  basicText: {
+    fontSize: 20,
+    color: 'lightblue',
+  } as RN.TextStyle,
+});
