7e37b24e764dbaf4e80f1a733ae5de838d7185f2
lukasz.mecfal <lukasz.mecfal@hycom.pl>
2020-08-12 10:13:56 +0200

    updated rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,19 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
     'object-curly-spacing': ['error', 'always'],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        'groups': [
+          'builtin',
+          'external',
+          'internal',
+        ],
+      },
+    ],
   },
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import * as RN from 'react-native';
+import React from 'react';
 
 import styles from './Home.style';
 
